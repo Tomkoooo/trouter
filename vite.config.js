efbab8204da7e33ee006df642d76dcffacd4809d
tomkoooo <patyivok8@gmail.com>
2024-11-10 00:08:44 +0100

    entry point fail

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     lib: {
       entry: 'src/index.ts',
       name: 'DynamicRouter',
-      fileName: (format) => `dynamic-router.${format}.js`
+      fileName: (format) => `index.${format}.js`
     },
     rollupOptions: {
       external: ['react', 'react-dom'], // Ezektől nem fogunk bundle-t készíteni
